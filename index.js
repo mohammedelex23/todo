3d607b06d47a627a8950533df7f65a7b2aaaf257
mohammed ismail <warethco@gmail.com>
2020-08-01 21:36:51 +0200

    with mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
+
+mongoose.connect('mongodb://localhost:27017/todoListDB', {useUnifiedTopology: true, useNewUrlParser: true}, (err) => {
+    err ? console.log(err) : console.log('Seccessfully conected to mongoDB');
+})
+
+// Task Schema
+const taskSchema = mongoose.Schema({
+    name: String
+})
+
+// Task model
+const Task = mongoose.model('Task', taskSchema)
 
 var options = {  weekday: 'long', month: 'long', day: 'numeric'};
 var curruntDate = new Date().toLocaleTimeString('en-us', options);
@@ -16,17 +29,33 @@ var month = curruntDate[1];
 
 curruntDate = `${weekDay}, ${month}`;
 
-var list = []
+
 
 app.get('/', (req, res) => {
-    res.render('home', {date : curruntDate, list: list})
+
+    Task.find((err, list) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('home', {date : curruntDate, list: list})
+        }
+    })
 })
 
 app.post('/', (req, res) => {
-    list.push(
-        {"name" : req.body.task}
-    )
-    res.redirect('/')
+    
+    var task = new Task({
+        name: req.body.task
+    })
+
+    task.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Seccessfully saved the task');
+            res.redirect('/')
+        }
+    })
 })
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
